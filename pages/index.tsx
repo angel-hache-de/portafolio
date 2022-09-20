78e56748bc18415ce44c7fdbe3315c650d981baf
angel-hache-de <angelhachede9678@gmail.com>
2022-09-20 09:48:59 -0500

    Removed unnecessary imports from index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,14 @@
 import { useEffect, useState } from "react";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+
 import Header from "../components/Header";
 import ShortInfoPhoto from "../components/ShortInfoPhoto";
 
-import Blob from "../rrrainbow.svg";
-import Shape from "../ssshape.svg";
 import animations from "../styles/Animations.module.css";
-import styles from "../styles/Home.module.css";
+
 import About from "../components/About";
 import Projects from "../components/Projects";
-import { Background, Parallax } from "react-parallax";
-import SocialMedia from "../components/SocialMedia";
-import ContactForm from "../components/ContactForm";
+
 import ContactSection from "../components/ContactSection";
 import Footer from "../components/Footer";
 
@@ -38,7 +33,9 @@ const Home: NextPage = () => {
       <Header showBackColor={scrollPosition > 30} />
 
       <div className="overflow-x-hidden -my-12">
-        <section className={`min-h-screen w-screen relative pt-10 flex items-center`}>
+        <section
+          className={`min-h-screen w-screen relative pt-10 flex items-center`}
+        >
           <div className="absolute top-0 left-0 -z-10 h-full w-full overflow-hidden">
             <div className={animations.bubbles}>
               <img src="/rrrainbow.svg" alt="back" className="w-full" />
