c9782e6631bedf5d980a2c1b308b277468f110a4
angel-hache-de <84556424+angel-hache-de@users.noreply.github.com>
2022-09-26 18:56:53 +0000

    Deploying to gh-pages from @ angel-hache-de/portafolio@c5de1bf8ecb9004204a8ee80b6d684cede531aa0 🚀

--- _next/static/OpNFffku_QNFxhBKrUd1W/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/307-21ca34d5d4226c7f.js","static/css/6400cd07b7b66452.css","static/chunks/pages/index-009898c936bb489b.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
